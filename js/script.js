0e5673ab2775f3e26db31fa19eb81a601c00c0aa
lacnoskillz <kaikane3457@hotmail.com>
2022-11-17 19:03:14 -0600

    added basic youtubeplayer to show up in a created div.  need to add listeners for user input

--- js/script.js
+++ js/script.js
@@ -49,6 +49,48 @@ async function searchMovies() {
 
 
 }
+//kais youtube section
 
+ // 2. This code loads the IFrame Player API code asynchronously.
+ var tag = document.createElement('script');
+ tag.src = "https://www.youtube.com/iframe_api";
+ console.log(tag.src);
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+ // 3. This function creates an <iframe> (and YouTube player)
+ //    after the API code downloads.
+ var player;
+ function onYouTubeIframeAPIReady() {
+   player = new YT.Player('divname', {
+     height: '390',
+     width: '640',
+     //change videoid to change what video plays
+     videoId: '5XkKA3_Qtz4',
+     playerVars: {
+       'playsinline': 1
+     },
+     events: {
+       'onReady': onPlayerReady,
+       'onStateChange': onPlayerStateChange
+     }
+   });
+ }
+ // 4. The API will call this function when the video player is ready.
+ function onPlayerReady(event) {
+   event.target.playVideo();
+ }
+ // 5. The API calls this function when the player's state changes.
+ //    The function indicates that when playing a video (state=1),
+ //    the player should play for six seconds and then stop.
+ var done = false;
+ function onPlayerStateChange(event) {
+   if (event.data == YT.PlayerState.PLAYING && !done) {
+     setTimeout(stopVideo, 6000);
+     done = true;
+   }
+ }
+ function stopVideo() {
+   player.stopVideo();
+ }
 
 
