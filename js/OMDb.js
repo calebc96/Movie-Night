9aa142495bd988554fb7acf1b1990eb96d8968b4
lacnoskillz <kaikane3457@hotmail.com>
2022-11-25 12:11:16 -0600

    started history function to save to local storage user search history

--- js/OMDb.js
+++ js/OMDb.js
@@ -35,9 +35,11 @@ async function searchMovies(event) {
 // NEW --------------------------------------------------------------------------------------------
     // inserting api information into empty div with an id of result-movie
     // need a placeholder image for if poster does not exist
+    userinput = data.Title;
+    kaismegamoviefunction(userinput)
     movieGrid.innerHTML = `
     <div class = "movie-poster">
-        <img src = "${(data.Poster != "N/A") ? data.Poster : "image_not_found.png"}" alt = "movie poster">
+        <img src = "${(data.Poster != "N/A") ? data.Poster : "image_not_found.png"}" alt = "movie poster" id="imgvid">
     </div>
     <div class = "movie-info">
         <h3 class = "movie-title">${data.Title}</h3>
@@ -101,7 +103,75 @@ async function searchMovieCollection() {
 
 }
 // NEW --------------------------------------------------------------------------------------------
-
+function kaismegamoviefunction(userinput) {
+    console.log("this function is running!");
+    if(userinput == undefined){
+      return;
+    }
+     let key = "AIzaSyCY_952gGjBqylPvw16_rgi2pB2NI6aoPk";
+   //chases key let key = "AIzaSyCNirqDt4O3qnoMFaSPZu1XhhRuMhZmjIQ";
+    let newyoutubeURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=" + userinput + "movietrailer&key=" + key;
+  
+    console.log("this is the new URL", newyoutubeURL);
+  
+    
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    console.log(tag.src);
+  
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    onYouTubeIframeAPIReady();
+    function onYouTubeIframeAPIReady() {
+  
+      fetch(newyoutubeURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+            if(player != undefined){
+                player.destoy();
+            }
+          console.log("youtube", data);
+          console.log("vid id", data.items[0].id.videoId);
+          return data.items[0].id.videoId
+        }).then(function (videoId) {
+            
+          player = new YT.Player('utube', {
+            height: '400',
+            width: 'auto',
+            //change videoid to change what video plays
+            videoId: videoId,
+            playerVars: {
+              'playsinline': 1
+            },
+            events: {
+              'onReady': onPlayerReady,
+              'onStateChange': onPlayerStateChange
+            }
+          });
+        });
+    }
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+      event.target.playVideo();
+      //player.pauseVideo();
+    }
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    var done = false;
+    function onPlayerStateChange(event) {
+      if (event.data == YT.PlayerState.PLAYING && !done) {
+  
+        done = true;
+   
+    }
+}
+}
 
 
 
